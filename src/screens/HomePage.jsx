77fd59847ed49169f5f157935d0ede5fc84300b9
hajar258 <hajar.mohammad285@gmail.com>
2023-10-07 13:22:47 +0300

    CREATED: Header

---
+++ src/screens/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header/>
+      <h1>Welcome to My Website</h1>
+      <p>This is the home page of my website.</p>
+      <p>Feel free to explore and enjoy!</p>
+    </div>
+  );
+};
+
+export default HomePage;
