071cdc06ac6cee91f0de13206b4b68f62b0e24cd
Tamoiselle <fate.alawi@gmail.com>
2023-10-07 15:48:51 +0300

    ADDED: Custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: { colors: {
+      violet:{
+        500:'#210535',
+        400:'#430d4b',
+        300:'#7b337d',
+        200:'#c874b2',
+        100:'#f5d5e0'
+      }
+    },},
+  },
+  
+  plugins: [],
+}
+
