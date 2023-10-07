1ed59bd3c9495a45f4f3abf648b2a92665afdf44
tasnim <tasnim0tantawi@gmail.com>
2023-10-07 16:05:14 +0300

    router

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -20,14 +20,7 @@ function AppRouter() {
                 <Route path="/" element={<Home />} />
                 <Route path={"/LogIn"} element={<LoginPage />} />
                 <Route path={"/SignUp"} element={<SignupPage />} />
-
-                <Route path={"/ListOfCourses"} element={<ListOfCourses />} />
-                <Route path={"/MyCourses"} element={<MyCourses />} />
-                <Route path={"/SeeMore"} element={<SeeMore />} />
-                <Route path={"/AboutUs"} element={<AboutUs />} />
-                <Route path={"/UpdateCourse"} element={<UpdateCourse />} />
-                <Route path={"/CreateCourse"} element={<CreateCourse />} />
-                <Route path={"/AddIns"} element={<AddIns />} />
+                >
                 <Route path="*" element={<h1>404: Not Found</h1>} />
             </Routes>
         </BrowserRouter>
